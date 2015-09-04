585ec48b4bef7173680af1e75b4a316cb53f7bb2
Matt Hoffman <hofmadresu@gmail.com>
2015-09-04 13:06:59 -0400

    Add basic view on iOS and adjust row height for OS specific needs

---
+++ MakeMeMove/MakeMeMove.iOS/ExerciseServiceManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MakeMeMove.iOS;
+using MakeMeMove.Model;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ExerciseServiceManager))]
+namespace MakeMeMove.iOS
+{
+    public class ExerciseServiceManager : IServiceManager
+    {
+        public void StartNotificationService(ExerciseSchedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopNotificationService(ExerciseSchedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
