ad137ac7ae9029a7e9e5fc3661f7440c754c2b9e
Matt Hoffman <hofmadresu@gmail.com>
2016-01-29 16:27:58 -0500

    Add show/hide functionality for custom exercise name. default to hidden

--- MakeMeMove/MakeMeMove.Droid/Activities/ManageExerciseActivity.cs
+++ MakeMeMove/MakeMeMove.Droid/Activities/ManageExerciseActivity.cs
@@ -19,6 +19,7 @@ namespace MakeMeMove.Droid.Activities
     public class ManageExerciseActivity : Activity
     {
         private Spinner _exerciseTypeSpinner;
+        private EditText _customExerciseNameText;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -27,6 +28,7 @@ namespace MakeMeMove.Droid.Activities
             SetContentView(Resource.Layout.ManageExercise);
 
             _exerciseTypeSpinner = FindViewById<Spinner>(Resource.Id.ExerciseTypeSpinner);
+            _customExerciseNameText = FindViewById<EditText>(Resource.Id.CustomExerciseNameBox);
 
             InitializePickers();
         }
@@ -34,8 +36,22 @@ namespace MakeMeMove.Droid.Activities
         private void InitializePickers()
         {
             var exerciseList = (from PreBuiltExersises suit in Enum.GetValues(typeof(PreBuiltExersises)) select suit.Humanize()).ToList();
-
             _exerciseTypeSpinner.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, exerciseList);
+
+            _exerciseTypeSpinner.ItemSelected += (sender, args) => ShowHideCustomText();
+        }
+
+        private void ShowHideCustomText()
+        {
+            var selectedExerciseType = (PreBuiltExersises)Enum.Parse(typeof (PreBuiltExersises), ((string) _exerciseTypeSpinner.SelectedItem).Dehumanize());
+            if (selectedExerciseType == PreBuiltExersises.Custom)
+            {
+                _customExerciseNameText.Visibility = ViewStates.Visible;
+            }
+            else
+            {
+                _customExerciseNameText.Visibility = ViewStates.Gone;
+            }
         }
     }
 }
