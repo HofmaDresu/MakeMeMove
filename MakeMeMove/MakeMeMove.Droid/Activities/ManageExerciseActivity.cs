03f2b973c41017318df7be8774bf30ab1549f884
Matt Hoffman <hofmadresu@gmail.com>
2016-01-29 16:16:32 -0500

    Add data to exercise type spinner

--- MakeMeMove/MakeMeMove.Droid/Activities/ManageExerciseActivity.cs
+++ MakeMeMove/MakeMeMove.Droid/Activities/ManageExerciseActivity.cs
@@ -10,17 +10,32 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Humanizer;
+using MakeMeMove.Model;
 
 namespace MakeMeMove.Droid.Activities
 {
     [Activity(Label = "ManageExerciseActivity", ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize)]
     public class ManageExerciseActivity : Activity
     {
+        private Spinner _exerciseTypeSpinner;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.ManageExercise);
+
+            _exerciseTypeSpinner = FindViewById<Spinner>(Resource.Id.ExerciseTypeSpinner);
+
+            InitializePickers();
+        }
+
+        private void InitializePickers()
+        {
+            var exerciseList = (from PreBuiltExersises suit in Enum.GetValues(typeof(PreBuiltExersises)) select suit.Humanize()).ToList();
+
+            _exerciseTypeSpinner.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, exerciseList);
         }
     }
 }
