e5ca9ff5d4ad0cface94e9de39634c84e8f339cc
Matt Hoffman <hofmadresu@gmail.com>
2016-06-24 16:32:49 -0400

    Remove obsolete iOS project. Add new iOS project

--- MakeMeMove/MakeMeMove.iOS/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace MakeMeMove.iOS
-{
-    public static class Constants
-    {
-        public static string SharedPreferencesKey = "MMM_Preferences";
-        public static string ServiceIsStartedKey = "ServiceIsStarted";
-        public static string ExerciseSchedule = "ExerciseSchedule";
-    }
-}
