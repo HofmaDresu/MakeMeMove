6c542ab3d437ec4a09b225257f00f408e6cb7bc9
Matt Hoffman <hofmadresu@gmail.com>
2016-07-01 11:51:45 -0400

    Add very basic layout for main screens + first nav

---
+++ MakeMeMove/MakeMeMove.iOS/ManageScheduleController.cs
@@ -0,0 +1,25 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MakeMeMove.iOS
+{
+	public partial class ManageScheduleController : UIViewController, IUINavigationBarDelegate
+    {
+        public ManageScheduleController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			NavBar.Delegate = this;
+		}
+
+		[Export("positionForBar:")]
+		public UIBarPosition PositionForBar(UIBarPositioning id)
+		{
+			return UIBarPosition.TopAttached;
+		}
+    }
+}
