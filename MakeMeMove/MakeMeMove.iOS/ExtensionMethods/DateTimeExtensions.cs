202a7dec50a554b2464ca5bcb3604eb6b11be333
Matt Hoffman <hofmadresu@gmail.com>
2016-08-09 19:36:03 -0400

    Remove obsolete line

--- MakeMeMove/MakeMeMove.iOS/ExtensionMethods/DateTimeExtensions.cs
+++ MakeMeMove/MakeMeMove.iOS/ExtensionMethods/DateTimeExtensions.cs
@@ -7,8 +7,6 @@ namespace MakeMeMove.iOS.ExtensionMethods
 	{
 		public static NSDate ToNSDate(this DateTime date)
 		{
-			var reference = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(2001, 1, 1, 0, 0, 0));
-
 			var dtBasis = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
 
