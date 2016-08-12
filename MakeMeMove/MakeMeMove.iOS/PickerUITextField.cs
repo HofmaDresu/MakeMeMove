028ad285a74ac9d73691b2c7760f35d417373c0d
Matt Hoffman <hofmadresu@gmail.com>
2016-08-12 14:53:27 -0400

    Fix style. Add code for PickerUITextField

--- MakeMeMove/MakeMeMove.iOS/PickerUITextField.cs
+++ MakeMeMove/MakeMeMove.iOS/PickerUITextField.cs
@@ -1,13 +1,19 @@
 using Foundation;
 using System;
 using UIKit;
+using ObjCRuntime;
 
 namespace MakeMeMove.iOS
 {
-    public partial class PickerUITextField : UITextField
-    {
-        public PickerUITextField (IntPtr handle) : base (handle)
-        {
-        }
-    }
+	partial class PickerUITextField : UITextField
+	{
+		public PickerUITextField(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override bool CanPerform(Selector action, NSObject withSender)
+		{
+			return false;
+		}
+	}
 }
