89ab95caa8d8d1b06d7ead35d648fd538c30feb7
Matt Hoffman <hofmadresu@gmail.com>
2016-08-12 14:22:29 -0400

    Start laying out Manage Exercise

---
+++ MakeMeMove/MakeMeMove.iOS/PickerUITextField.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MakeMeMove.iOS
+{
+    public partial class PickerUITextField : UITextField
+    {
+        public PickerUITextField (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
