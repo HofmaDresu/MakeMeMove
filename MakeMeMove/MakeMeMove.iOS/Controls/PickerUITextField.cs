c9ca8fe991cceebc294766c7cbcf73692b33f2df
Matt Hoffman <hofmadresu@gmail.com>
2016-08-28 18:37:06 -0400

    Add down arrows for picker textboxes

--- MakeMeMove/MakeMeMove.iOS/Controls/PickerUITextField.cs
+++ MakeMeMove/MakeMeMove.iOS/Controls/PickerUITextField.cs
@@ -2,6 +2,8 @@ using Foundation;
 using System;
 using UIKit;
 using ObjCRuntime;
+using CoreGraphics;
+using MakeMeMove.iOS.Helpers;
 
 namespace MakeMeMove.iOS
 {
@@ -9,6 +11,16 @@ namespace MakeMeMove.iOS
 	{
 		public PickerUITextField(IntPtr handle) : base(handle)
 		{
+			var foo = "▼";
+			var arrowButton = new UIButton(new CGRect(0, 0, 20, 20));
+			arrowButton.TranslatesAutoresizingMaskIntoConstraints = false;
+			arrowButton.SetTitle(foo, UIControlState.Normal);
+			arrowButton.SetTitleColor(FudistColors.InteractableTextColor, UIControlState.Normal);
+			arrowButton.UserInteractionEnabled = false;
+			AddSubview(arrowButton);
+
+			arrowButton.RightAnchor.ConstraintGreaterThanOrEqualTo(this.RightAnchor, 0).Active = true;
+			arrowButton.CenterYAnchor.ConstraintEqualTo(this.CenterYAnchor).Active = true;
 		}
 
 		public override bool CanPerform(Selector action, NSObject withSender)
