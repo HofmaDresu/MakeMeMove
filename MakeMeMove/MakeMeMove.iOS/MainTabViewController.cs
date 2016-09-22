b571f06472bd28514bdac7943b667aaceaefb8d2
Matt Hoffman <hofmadresu@gmail.com>
2016-09-22 13:08:10 -0400

    Adjust segues and add code for menu gesture

---
+++ MakeMeMove/MakeMeMove.iOS/MainTabViewController.cs
@@ -0,0 +1,24 @@
+using Foundation;
+using System;
+using UIKit;
+using SWRevealViewControllerBinding;
+
+namespace MakeMeMove.iOS
+{
+    public partial class MainTabViewController : UITabBarController
+    {
+        public MainTabViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			if (this.RevealViewController() == null)
+				return;
+
+			//RevealButtonItem.Clicked += (sender, e) => this.RevealViewController().RevealToggleAnimated(true);
+			View.AddGestureRecognizer(this.RevealViewController().PanGestureRecognizer);
+		}
+    }
+}
