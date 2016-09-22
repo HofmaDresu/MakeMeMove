154bb7041236fbb0838742170b68733c3ed5ccfe
Matt Hoffman <hofmadresu@gmail.com>
2016-09-22 13:57:18 -0400

    Ad menu view controller. Increase "add exercise" button size

---
+++ MakeMeMove/MakeMeMove.iOS/MenuViewController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace MakeMeMove.iOS
+{
+    public partial class MenuViewController : UIViewController
+    {
+        public MenuViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
