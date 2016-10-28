590eb7d58644df8e7719b726784d797b8ffd9041
Matt Hoffman <hofmadresu@gmail.com>
2016-10-28 16:38:47 -0400

    Add nav to Fudist Login. Tint back button white

--- MakeMeMove/MakeMeMove.iOS/ViewControllers/FudistLoginViewController.cs
+++ MakeMeMove/MakeMeMove.iOS/ViewControllers/FudistLoginViewController.cs
@@ -1,13 +1,22 @@
 ﻿using Foundation;
 using System;
+using MakeMeMove.iOS.Helpers;
+using MakeMeMove.iOS.ViewControllers.Base;
 using UIKit;
 
 namespace MakeMeMove.iOS
 {
-    public partial class FudistLoginViewController : UIViewController
+    public partial class FudistLoginViewController : BaseViewController
     {
         public FudistLoginViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            NavigationController.NavigationBar.TintColor = UIColor.White;
         }
     }
 }
