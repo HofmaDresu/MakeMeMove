5559a116386cbc79acea80e34f9ee3971be34d05
Matt Hoffman <hofmadresu@gmail.com>
2016-10-28 16:25:29 -0400

    add initial fudist login view controller

---
+++ MakeMeMove/MakeMeMove.iOS/ViewControllers/FudistLoginViewController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace MakeMeMove.iOS
+{
+    public partial class FudistLoginViewController : UIViewController
+    {
+        public FudistLoginViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
