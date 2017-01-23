735e1c7d0b346f02f463a6cc07f93ab2102bb593
Matt Hoffman <matt@fudi.st>
2017-01-23 11:38:15 -0500

    Remove obsolete methods

--- MakeMeMove/MakeMeMove/TickUtility.cs
+++ MakeMeMove/MakeMeMove/TickUtility.cs
@@ -71,25 +71,6 @@ namespace MakeMeMove
             return GetStartNextDayIfOverTodaysEnd(schedule, fromDateValue.AddMinutes(minuteInterval));
         }
 
-        private static DateTime GetNextHalfHourlyRun(ExerciseSchedule schedule, DateTime fromDateValue)
-        {
-            var targetDate = fromDateValue.Minute < 30 
-                ? new DateTime(fromDateValue.Year, fromDateValue.Month, fromDateValue.Day, fromDateValue.Hour, 30, 0) 
-                : ZeroOutMinutesAndLower(fromDateValue).AddHours(1);
-
-            return GetStartNextDayIfOverTodaysEnd(schedule, targetDate);
-        }
-
-        private static DateTime GetNextHourlyRun(ExerciseSchedule schedule, DateTime fromDateValue)
-        {
-            return GetStartNextDayIfOverTodaysEnd(schedule, ZeroOutMinutesAndLower(fromDateValue.AddHours(1)));
-        }
-
-        private static DateTime GetNextBiHourlyRun(ExerciseSchedule schedule, DateTime fromDateValue)
-        {
-            return GetStartNextDayIfOverTodaysEnd(schedule, ZeroOutMinutesAndLower(fromDateValue.AddHours(2)));
-        }
-
         private static DateTime GetStartNextDayIfOverTodaysEnd(ExerciseSchedule schedule, DateTime target)
         {
             if (target.TimeOfDay <= schedule.EndTime.TimeOfDay && target.TimeOfDay >= schedule.StartTime.TimeOfDay)
@@ -116,16 +97,6 @@ namespace MakeMeMove
         private static DateTime GetStartOfToday(ExerciseSchedule schedule, DateTime fromDateValue)
         {
             return new DateTime(fromDateValue.Year, fromDateValue.Month, fromDateValue.Day, schedule.StartTime.Hour, schedule.StartTime.Minute, schedule.StartTime.Second);
-        }
-
-        private static DateTime ZeroOutMinutesAndLower(DateTime dateTime)
-        {
-            return ZeroOutSecondsAndLower(dateTime).AddMinutes(-1 * dateTime.Minute);
-        }
-
-        private static DateTime ZeroOutSecondsAndLower(DateTime dateTime)
-        {
-            return dateTime.AddSeconds(-1 * dateTime.Second).AddMilliseconds(-1 * dateTime.Millisecond);
         }
     }
 }
