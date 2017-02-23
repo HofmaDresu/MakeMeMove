4a230e54ddf6bd20d88481a8c2855380d29c4b03
Matt Hoffman <matt@fudi.st>
2017-02-23 15:54:25 -0500

    Implement back button. Style settings screen

---
+++ MakeMeMove/MakeMeMove.iOS/ViewControllers/SettingsViewController.cs
@@ -0,0 +1,53 @@
+﻿using CoreGraphics;
+using Foundation;
+using MakeMeMove.iOS.Helpers;
+using MakeMeMove.iOS.ViewControllers.Base;
+using System;
+using UIKit;
+
+namespace MakeMeMove.iOS
+{
+    public partial class SettingsViewController : BaseViewController
+    {
+        public SettingsViewController (IntPtr handle) : base (handle)
+        {
+            ScreenName = "Settings";
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            NavBar.Translucent = false;
+            NavBar.BarTintColor = FudistColors.PrimaryColor;
+            NavBar.TitleTextAttributes = new UIStringAttributes
+            {
+                ForegroundColor = UIColor.White
+            };
+
+            var statusBarColor = new UIView(new CGRect(0, 0, View.Frame.Width, 20))
+            {
+                BackgroundColor = FudistColors.PrimaryColor
+            };
+            View.Add(statusBarColor);
+
+            BackButton.Clicked += BackButton_Clicked;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            BackButton.TintColor = UIColor.White;
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            BackButton.Clicked -= BackButton_Clicked;
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+            DismissViewController(true, () => { });
+        }
+    }
+}
---
+++ MakeMeMove/MakeMeMove.iOS/ViewControllers/SettingsViewController.designer.cs
@@ -0,0 +1,38 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace MakeMeMove.iOS
+{
+    [Register ("SettingsViewController")]
+    partial class SettingsViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem BackButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UINavigationBar NavBar { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (BackButton != null) {
+                BackButton.Dispose ();
+                BackButton = null;
+            }
+
+            if (NavBar != null) {
+                NavBar.Dispose ();
+                NavBar = null;
+            }
+        }
+    }
+}
