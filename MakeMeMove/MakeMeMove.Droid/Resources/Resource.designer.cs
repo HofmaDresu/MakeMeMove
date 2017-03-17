770cea7102dc5fd8244dd194c8115f77c8752b84
Matt Hoffman <matt@fudi.st>
2017-03-17 12:44:26 -0400

    Add initial totals screen

--- MakeMeMove/MakeMeMove.Droid/Resources/Resource.designer.cs
+++ MakeMeMove/MakeMeMove.Droid/Resources/Resource.designer.cs
@@ -2414,64 +2414,67 @@ namespace MakeMeMove.Droid
 			public const int ExerciseListItem = 2130903068;
 			
 			// aapt resource value: 0x7f03001d
-			public const int LoadingOverlay = 2130903069;
+			public const int ExerciseTotals = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int LogIn = 2130903070;
+			public const int LoadingOverlay = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int Main = 2130903071;
+			public const int LogIn = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int Main_ContentArea = 2130903072;
+			public const int Main = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int Main_ExerciseList = 2130903073;
+			public const int Main_ContentArea = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int Main_ExerciseSchedule = 2130903074;
+			public const int Main_ExerciseList = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int ManageExercise = 2130903075;
+			public const int Main_ExerciseSchedule = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int ManageSchedule = 2130903076;
+			public const int ManageExercise = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int notification_media_action = 2130903077;
+			public const int ManageSchedule = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int notification_media_cancel_action = 2130903078;
+			public const int notification_media_action = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_template_big_media = 2130903079;
+			public const int notification_media_cancel_action = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_template_big_media_narrow = 2130903080;
+			public const int notification_template_big_media = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int notification_template_lines = 2130903081;
+			public const int notification_template_big_media_narrow = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int notification_template_media = 2130903082;
+			public const int notification_template_lines = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_template_part_chronometer = 2130903083;
+			public const int notification_template_media = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int notification_template_part_time = 2130903084;
+			public const int notification_template_part_chronometer = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int select_dialog_item_material = 2130903085;
+			public const int notification_template_part_time = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int select_dialog_multichoice_material = 2130903086;
+			public const int select_dialog_item_material = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int select_dialog_singlechoice_material = 2130903087;
+			public const int select_dialog_multichoice_material = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int support_simple_spinner_dropdown_item = 2130903088;
+			public const int select_dialog_singlechoice_material = 2130903088;
+			
+			// aapt resource value: 0x7f030031
+			public const int support_simple_spinner_dropdown_item = 2130903089;
 			
 			static Layout()
 			{
