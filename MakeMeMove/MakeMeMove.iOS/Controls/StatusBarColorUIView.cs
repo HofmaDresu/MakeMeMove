fb9ad672451a109c680f1b7f6dbf4e6340fafc19
Matt Hoffman <hofmadresu@gmail.com>
2018-03-24 12:14:27 -0400

    Update hack for the notch

--- MakeMeMove/MakeMeMove.iOS/Controls/StatusBarColorUIView.cs
+++ MakeMeMove/MakeMeMove.iOS/Controls/StatusBarColorUIView.cs
@@ -8,9 +8,22 @@ namespace MakeMeMove.iOS.Controls
 	public class StatusBarColorUIView : UIView
     {
 		public StatusBarColorUIView(nfloat width)
-			: base(new CGRect(0, 0, width, 20))
+			: base(new CGRect(0, 0, width, GetHeight()))
 		{
 			BackgroundColor = Colors.PrimaryColor;
+		}
+        
+        private static nfloat GetHeight() 
+		{
+			var mainWindow = UIApplication.SharedApplication.Delegate.GetWindow();
+			if(UIDevice.CurrentDevice.CheckSystemVersion(11, 0) && mainWindow.SafeAreaInsets.Top > 0) 
+			{
+				return 50;
+			}
+			else
+			{
+				return 20;
+			}
 		}
     }
 }
