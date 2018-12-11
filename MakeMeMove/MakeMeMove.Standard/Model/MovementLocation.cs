9e31573546705f75d33564e7780b5dcbc02f857b
Matt Hoffman <hofmadresu@gmail.com>
2018-12-11 17:33:09 -0500

    Add db class and methods for MovementLocations

---
+++ MakeMeMove/MakeMeMove.Standard/Model/MovementLocation.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace MakeMeMove.Standard.Model
+{
+    [Table("MovementLocations")]
+    public class MovementLocation
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public (double Latitude, double Longitude) GeoCoordinate { get; set; }
+    }
+}
